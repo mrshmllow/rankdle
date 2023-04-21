a06666ab28d14d9ecc85200759bb8bb2295a5ad3
marshmallow <marshycity@gmail.com>
2023-04-21 13:44:59 +1000

    add /submit

--- db_types.d.ts
+++ db_types.d.ts
@@ -4,82 +4,105 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json }
-  | Json[]
+  | Json[];
 
 export interface Database {
   public: {
     Tables: {
       guesses: {
         Row: {
-          clip_id: number
-          created_at: string | null
-          id: number
-          rank: Database["public"]["Enums"]["valorant_rank"]
-        }
+          clip_id: number;
+          created_at: string | null;
+          id: number;
+          rank: Database["public"]["Enums"]["valorant_rank"];
+        };
         Insert: {
-          clip_id: number
-          created_at?: string | null
-          id?: number
-          rank: Database["public"]["Enums"]["valorant_rank"]
-        }
+          clip_id: number;
+          created_at?: string | null;
+          id?: number;
+          rank: Database["public"]["Enums"]["valorant_rank"];
+        };
         Update: {
-          clip_id?: number
-          created_at?: string | null
-          id?: number
-          rank?: Database["public"]["Enums"]["valorant_rank"]
-        }
-      }
+          clip_id?: number;
+          created_at?: string | null;
+          id?: number;
+          rank?: Database["public"]["Enums"]["valorant_rank"];
+        };
+      };
+      proposed: {
+        Row: {
+          created_at: string | null;
+          id: number;
+          tracker_id: string;
+          val_id: string;
+          youtube_id: string;
+        };
+        Insert: {
+          created_at?: string | null;
+          id?: number;
+          tracker_id: string;
+          val_id: string;
+          youtube_id: string;
+        };
+        Update: {
+          created_at?: string | null;
+          id?: number;
+          tracker_id?: string;
+          val_id?: string;
+          youtube_id?: string;
+        };
+      };
       rankdles: {
         Row: {
-          approved: boolean
-          created_at: string | null
-          id: number
-          rank: Database["public"]["Enums"]["valorant_rank"]
-          tracker_match: string
-          youtube_id: string
-        }
+          approved: boolean;
+          created_at: string | null;
+          id: number;
+          rank: Database["public"]["Enums"]["valorant_rank"];
+          tracker_match: string;
+          youtube_id: string;
+        };
         Insert: {
-          approved?: boolean
-          created_at?: string | null
-          id?: number
-          rank: Database["public"]["Enums"]["valorant_rank"]
-          tracker_match: string
-          youtube_id: string
-        }
+          approved?: boolean;
+          created_at?: string | null;
+          id?: number;
+          rank: Database["public"]["Enums"]["valorant_rank"];
+          tracker_match: string;
+          youtube_id: string;
+        };
         Update: {
-          approved?: boolean
-          created_at?: string | null
-          id?: number
-          rank?: Database["public"]["Enums"]["valorant_rank"]
-          tracker_match?: string
-          youtube_id?: string
-        }
-      }
-    }
+          approved?: boolean;
+          created_at?: string | null;
+          id?: number;
+          rank?: Database["public"]["Enums"]["valorant_rank"];
+          tracker_match?: string;
+          youtube_id?: string;
+        };
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
       get_daily_rankdles: {
-        Args: Record<PropertyKey, never>
+        Args: Record<PropertyKey, never>;
         Returns: {
-          id: number
-          youtube_id: string
-          rank: Database["public"]["Enums"]["valorant_rank"]
-          tracker_match: string
-          date: string
-        }[]
-      }
+          id: number;
+          youtube_id: string;
+          rank: Database["public"]["Enums"]["valorant_rank"];
+          tracker_match: string;
+          date: string;
+        }[];
+      };
       get_rank_distribution: {
         Args: {
-          p_clip_id: number
-        }
+          p_clip_id: number;
+        };
         Returns: {
-          rank_rankdle: Database["public"]["Enums"]["valorant_rank"]
-          percentage: number
-        }[]
-      }
-    }
+          rank_rankdle: Database["public"]["Enums"]["valorant_rank"];
+          percentage: number;
+        }[];
+      };
+    };
     Enums: {
       valorant_rank:
         | "iron"
@@ -90,10 +113,10 @@ export interface Database {
         | "diamond"
         | "ascendant"
         | "immortal"
-        | "radiant"
-    }
+        | "radiant";
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
