a56706d3c2d952e9ce3f4d9c9b84509b024c106b
marshmallow <marshycity@gmail.com>
2023-04-22 14:40:12 +1000

    add nice little pretty transitions to dialogs

--- app/PostGuessDialog.tsx
+++ app/PostGuessDialog.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import { Dialog } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 import { getImageData } from "@/lib/icons";
 import { Rank } from "@/lib/types";
 import { calculateStars } from "@/lib/stars";
 import pluralize from "pluralize";
-import { Suspense, useMemo } from "react";
+import { Fragment, Suspense, useMemo } from "react";
 import GuessDistribution from "./GuessDistribution";
 
 export default function PostGuessDialog({
@@ -28,81 +28,103 @@ export default function PostGuessDialog({
   );
 
   return (
-    <Dialog open={isOpen} onClose={onClose}>
-      <div className="fixed inset-0 bg-black/80" aria-hidden="true" />
+    <Transition show={isOpen} appear as={Fragment}>
+      <Dialog open={isOpen} onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/80" aria-hidden="true" />
+        </Transition.Child>
 
-      <div className="fixed inset-0 flex items-center justify-center">
-        <Dialog.Panel className="w-full max-w-2xl rounded-md bg-ctp-mantle p-2 space-y-2">
-          <Dialog.Title className="text-2xl font-bold">Result</Dialog.Title>
-
-          <div className="grid grid-flow-col gap-2">
-            <div>
-              <p className="text-center font-semibold">Your Guess</p>
-              <Image
-                src={getImageData(guess)}
-                alt={Rank[guess].toString()}
-                placeholder="blur"
-                width={70}
-                height={70}
-                className="mx-auto"
-              />
-            </div>
-
-            <div>
-              <p className="text-center font-semibold">Answer</p>
-              <Image
-                src={getImageData(answer)}
-                alt={Rank[answer].toString()}
-                placeholder="blur"
-                width={70}
-                height={70}
-                className="mx-auto"
-              />
-            </div>
-          </div>
-
-          <p className="text-center">
-            {gainedStars === 0 ? (
-              <>You got no stars</>
-            ) : (
-              <>
-                You gained{" "}
-                <strong>
-                  {gainedStars} {pluralize("star", gainedStars)}
-                </strong>
-              </>
-            )}
-          </p>
-
-          <Suspense
-            fallback={
-              <div className="h-60 w-full bg-ctp-crust animate-pulse" />
-            }
+        <div className="fixed inset-0 flex items-center justify-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
           >
-            <GuessDistribution clip_id={clip_id} />
-          </Suspense>
+            <Dialog.Panel className="w-full max-w-2xl rounded-md bg-ctp-mantle p-2 space-y-2">
+              <Dialog.Title className="text-2xl font-bold">Result</Dialog.Title>
 
-          <a
-            href={`https://tracker.gg/valorant/match/${tracker_id}`}
-            target="_blank"
-            className="rounded-md py-2 px-4 w-full transition-colors bg-ctp-surface0 text-ctp-text block text-center"
-          >
-            View Match on Tracker.gg
-          </a>
+              <div className="grid grid-flow-col gap-2">
+                <div>
+                  <p className="text-center font-semibold">Your Guess</p>
+                  <Image
+                    src={getImageData(guess)}
+                    alt={Rank[guess].toString()}
+                    placeholder="blur"
+                    width={70}
+                    height={70}
+                    className="mx-auto"
+                  />
+                </div>
 
-          <button
-            className={
-              "rounded-md py-2 px-4 w-full transition-colors bg-ctp-red text-ctp-base"
-            }
-            onClick={(e) => {
-              e.preventDefault();
-              onClose();
-            }}
-          >
-            Next
-          </button>
-        </Dialog.Panel>
-      </div>
-    </Dialog>
+                <div>
+                  <p className="text-center font-semibold">Answer</p>
+                  <Image
+                    src={getImageData(answer)}
+                    alt={Rank[answer].toString()}
+                    placeholder="blur"
+                    width={70}
+                    height={70}
+                    className="mx-auto"
+                  />
+                </div>
+              </div>
+
+              <p className="text-center">
+                {gainedStars === 0 ? (
+                  <>You got no stars</>
+                ) : (
+                  <>
+                    You gained{" "}
+                    <strong>
+                      {gainedStars} {pluralize("star", gainedStars)}
+                    </strong>
+                  </>
+                )}
+              </p>
+
+              <Suspense
+                fallback={
+                  <div className="h-60 w-full bg-ctp-crust animate-pulse" />
+                }
+              >
+                <GuessDistribution clip_id={clip_id} />
+              </Suspense>
+
+              <a
+                href={`https://tracker.gg/valorant/match/${tracker_id}`}
+                target="_blank"
+                className="rounded-md py-2 px-4 w-full transition-colors bg-ctp-surface0 text-ctp-text block text-center"
+              >
+                View Match on Tracker.gg
+              </a>
+
+              <button
+                className={
+                  "rounded-md py-2 px-4 w-full transition-colors bg-ctp-red text-ctp-base"
+                }
+                onClick={(e) => {
+                  e.preventDefault();
+                  onClose();
+                }}
+              >
+                Next
+              </button>
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
   );
 }
--- app/WelcomeDialog.tsx
+++ app/WelcomeDialog.tsx
@@ -1,5 +1,6 @@
-import { Dialog } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 import { StarIcon } from "@heroicons/react/24/solid";
+import { Fragment } from "react";
 
 export default function WelcomeDialog({
   isOpen,
@@ -9,46 +10,68 @@ export default function WelcomeDialog({
   onClose: () => void;
 }) {
   return (
-    <Dialog open={isOpen} onClose={onClose}>
-      <div className="fixed inset-0 bg-black/80" aria-hidden="true" />
+    <Transition show={isOpen} appear as={Fragment}>
+      <Dialog open={isOpen} onClose={onClose} as="div">
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/80" aria-hidden="true" />
+        </Transition.Child>
 
-      <div className="fixed inset-0 flex items-center justify-center">
-        <Dialog.Panel className="w-full max-w-2xl rounded-md bg-ctp-mantle p-2 space-y-2">
-          <Dialog.Title className="text-2xl font-bold">
-            How To Play
-          </Dialog.Title>
-
-          <p>
-            Guess the rank! Get atleast <strong>3</strong> out of{" "}
-            <strong>6</strong> stars to keep your daily streak.
-          </p>
-
-          <ul className="list-disc list-inside">
-            <li className="relative">
-              One Rank Off:
-              <StarIcon className="w-5 h-5 text-ctp-yellow inline-block ml-1 absolute top-1/2 -translate-y-1/2" />
-            </li>
-            <li className="relative">
-              Correct Guess:
-              <div className="inline-flex gap-1 absolute top-1/2 -translate-y-1/2 ml-1">
-                {[...Array(2)].map((_, i) => (
-                  <StarIcon
-                    key={i}
-                    className="w-5 h-5 text-ctp-yellow inline-block"
-                  />
-                ))}
-              </div>
-            </li>
-          </ul>
-
-          <button
-            className="rounded-md py-2 px-4 w-full bg-ctp-red text-ctp-base"
-            onClick={() => onClose()}
+        <div className="fixed inset-0 flex items-center justify-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
           >
-            Play
-          </button>
-        </Dialog.Panel>
-      </div>
-    </Dialog>
+            <Dialog.Panel className="w-full max-w-2xl rounded-md bg-ctp-mantle p-2 space-y-2">
+              <Dialog.Title className="text-2xl font-bold">
+                How To Play
+              </Dialog.Title>
+
+              <p>
+                Guess the rank! Get atleast <strong>3</strong> out of{" "}
+                <strong>6</strong> stars to keep your daily streak.
+              </p>
+
+              <ul className="list-disc list-inside">
+                <li className="relative">
+                  One Rank Off:
+                  <StarIcon className="w-5 h-5 text-ctp-yellow inline-block ml-1 absolute top-1/2 -translate-y-1/2" />
+                </li>
+                <li className="relative">
+                  Correct Guess:
+                  <div className="inline-flex gap-1 absolute top-1/2 -translate-y-1/2 ml-1">
+                    {[...Array(2)].map((_, i) => (
+                      <StarIcon
+                        key={i}
+                        className="w-5 h-5 text-ctp-yellow inline-block"
+                      />
+                    ))}
+                  </div>
+                </li>
+              </ul>
+
+              <button
+                className="rounded-md py-2 px-4 w-full bg-ctp-red text-ctp-base"
+                onClick={() => onClose()}
+              >
+                Play
+              </button>
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
   );
 }
