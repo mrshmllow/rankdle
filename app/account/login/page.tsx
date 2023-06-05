dab8e7d4b4f5cf5bf9f92c979e4b74f78f761284
marshmallow <marshycity@gmail.com>
2023-06-05 17:27:52 +1000

    sign in callback url

--- app/account/login/page.tsx
+++ app/account/login/page.tsx
@@ -3,9 +3,11 @@
 import { cx } from "cva";
 import { useState } from "react";
 import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
 
 export default function Login() {
   const [loading, setLoading] = useState(false);
+  const searchParams = useSearchParams();
 
   return (
     <main className="grid gap-2 max-w-2xl mx-auto px-2">
@@ -14,7 +16,11 @@ export default function Login() {
       <button
         onClick={async () => {
           setLoading(true);
-          signIn("discord");
+          signIn("discord", {
+            callbackUrl: searchParams?.get("callbackUrl")
+              ? searchParams?.get("callbackUrl")!
+              : "/",
+          });
         }}
         className={cx([
           "rounded-md py-2 px-4 w-full transition-colors",
